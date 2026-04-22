1c0b280409c5dc73e1a22f7340e3d94f0bc6346e
bluebirddevs <bluebirdsmailtime@gmail.com>
2026-04-21 22:28:57 -0700

    Fix previewIconHover not working on server lists Fix containerTitle not working on advancements screen

--- src/main/java/bluebird/recolor/mixin/IconHoverEntryMixin.java
+++ src/main/java/bluebird/recolor/mixin/IconHoverEntryMixin.java
@@ -1,15 +1,24 @@
 package bluebird.recolor.mixin;
 
 import bluebird.recolor.Colors;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
+import net.minecraft.client.gui.screen.pack.PackListWidget;
 import net.minecraft.client.gui.screen.world.WorldListWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(WorldListWidget.WorldEntry.class)
-public class WorldSelectionListMixin {
+@Mixin({
+        PackListWidget.ResourcePackEntry.class,
+        WorldListWidget.WorldEntry.class,
+        MultiplayerServerListWidget.ServerEntry.class
+})
+public class IconHoverEntryMixin {
     @ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;fill(IIIII)V"), index = 4)
     private int modifyColor(int color) {
-        return Colors.previewIconHover;
+        if (color == -1601138544) {
+            return Colors.previewIconHover;
+        }
+        return color;
     }
 }
